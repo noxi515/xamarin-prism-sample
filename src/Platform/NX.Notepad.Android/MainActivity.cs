c2877dfea0ad28cfe47d1cdc74c635ab060c42ca
noxi <tzo.ppl@gmail.com>
2017-10-30 22:37:45 +0900

    AndroidプロジェクトをXamarin Formsタイプへ

--- src/Platform/NX.Notepad.Android/MainActivity.cs
+++ src/Platform/NX.Notepad.Android/MainActivity.cs
@@ -1,19 +1,22 @@
 ﻿using Android.App;
-using Android.Widget;
+using Android.Content.PM;
 using Android.OS;
 
-namespace NX.Notepad.Android
+namespace NX.Notepad.Droid
 {
-    [Activity(Label = "NX.Notepad.Android", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "App1", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
         }
     }
 }
-
